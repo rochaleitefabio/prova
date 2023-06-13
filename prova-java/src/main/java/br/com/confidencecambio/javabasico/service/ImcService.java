1947e1fe5664d20324f595150df8533114a9b6ef
rochaleitefabio <104524024+rochaleitefabio@users.noreply.github.com>
2023-06-13 19:48:11 -0300

    Delete prova-java directory

--- prova-java/src/main/java/br/com/confidencecambio/javabasico/service/ImcService.java
+++
@@ -1,8 +0,0 @@
-package br.com.confidencecambio.javabasico.service;
-
-import br.com.confidencecambio.javabasico.dto.response.CalculoImcResponse;
-import br.com.confidencecambio.javabasico.model.Imc;
-
-public interface ImcService {
-    CalculoImcResponse calculoIMC(Imc model);
-}
